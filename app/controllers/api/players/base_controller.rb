6f7991b5f44995dfc75bbb9c8d71560c87d9e827
Lewis <lewisyoul@gmail.com>
2017-12-19 10:23:02 +0000

    create players api
    altered the existing rails app to be an API with nested attributes!

---
+++ app/controllers/api/players/base_controller.rb
@@ -0,0 +1,3 @@
+class Api::Players::BaseController < ApplicationController
+  respond_to :json
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times { Player.create!(name: "Marco", hp: 100, strength: 100)}
