c4ffd26348b5e5ef77f631b7400a1f4e2e245780
Tabish Raza <tabishraza@gmail.com>
2017-12-22 14:59:19 +0000

    router spits out player images

--- app/controllers/api/players/players_controller.rb
+++ app/controllers/api/players/players_controller.rb
@@ -1,6 +1,6 @@
 class Api::Players::PlayersController < Api::Players::BaseController
   def index
     players = Player.all
-    render json: players, include: 'actions'
+    render json: players, include: ['actions', 'images']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :players do
       resources :players, only: [:index]
+      resources :images, only: [:index]
     end
   end
 
